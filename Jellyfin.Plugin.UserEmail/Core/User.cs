ecddac116c187541dfcb1989f2c9318eac05c109
Péter Sturm <sturm.peti@gmail.com>
2020-09-30 16:42:45 +0200

    Created solution from jellyfin-plugin-template and added base classes

---
+++ Jellyfin.Plugin.UserEmail/Core/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Jellyfin.Plugin.UserEmail.Core
+{
+    internal class User
+    {
+        public string UserName { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ Jellyfin.Plugin.UserEmail/Core/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jellyfin.Plugin.UserEmail.Core
+{
+    internal class UserRepository
+    {
+        public IEnumerable<User> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateUserEmail(string userName, string emailAddress)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
