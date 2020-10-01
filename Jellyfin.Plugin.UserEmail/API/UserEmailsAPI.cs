619d15efa6b1b00cffeb6a776ce3f5263c9d890d
Péter Sturm <sturm.peti@gmail.com>
2020-10-01 10:44:51 +0200

    Created enpoint to get user emails

---
+++ Jellyfin.Plugin.UserEmail/API/UserEmailsAPI.cs
@@ -0,0 +1,34 @@
+﻿using Jellyfin.Plugin.UserEmail.Core;
+using MediaBrowser.Model.Services;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace Jellyfin.Plugin.UserEmail.API
+{
+    public class UserEmailsAPI
+    {
+        private readonly ILogger<UserEmailsAPI> _logger;
+        private readonly IUserRepository _userRepository;
+
+        public UserEmailsAPI(ILogger<UserEmailsAPI> logger, IUserRepository userRepository)
+        {
+            _logger = logger;
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> Get(GetUserEmails request)
+        {
+            _logger.LogInformation($"{nameof(GetUserEmails)} requested");
+            _logger.LogInformation("Sync users between Jellyfin db and plugin db");
+            _userRepository.SyncWithJellyfinUsers();
+            _logger.LogInformation("Sync finished");
+
+            return _userRepository.GetAll();
+        }
+    }
+
+    [Route("/Jellyfin.Plugin.UserEmail/GetUserEmails", verbs:"GET")]
+    public class GetUserEmails : IReturn<IEnumerable<User>>
+    { 
+    }
+}
